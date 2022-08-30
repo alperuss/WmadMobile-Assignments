5640a86e28f4e4c107adbc00d162ef3d57ba7d3a
Alper Us <alper.usss@gmail.com>
2022-08-29 18:46:31 -0700

    assignment4

---
+++ swift-assignment4/problem1.swift
@@ -0,0 +1,13 @@
+func main() {
+    var func1: (Int, Int) -> Int
+    var func2: (String) -> Void
+    let func3: () -> Int
+    let func4: () -> [() -> Void]
+    let func5: ([(Int) -> Void]) -> [String]
+    var func6: ([(Int) -> Void]) -> [(Int) -> Void]
+    var func7: ([(Int) -> Void]?) -> [(Int) -> Void]?
+    var func8: ([(Int) -> Void]?) -> [(Int) -> Void]?
+    var func9: () -> ((Int) -> Int, Int)?
+}
+
+main()
