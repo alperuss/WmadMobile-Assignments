7fbec36fcc1c7f3ef00be3f61c8f5a778f6ba6ec
Alper Us <alper.usss@gmail.com>
2022-10-07 22:06:13 -0700

    Assignment 8

---
+++ swift-assignment8/problem3.swift
@@ -0,0 +1,27 @@
+extension String {
+    var numberOfDigits: Int {
+        return self.count
+    }
+
+    var uppercase: String {
+        return self.uppercased()
+    }
+
+    func isPalindrome() -> Bool {
+        return self == reverseIt()
+    }
+
+    func reverseIt() -> String {
+        return String(self.reversed())
+    }
+}
+
+func main() {
+    let k = "kayak"
+    print(k.numberOfDigits)
+    print(k.uppercase)
+    print(k.isPalindrome())
+    print(k.reverseIt())
+}
+
+main()
